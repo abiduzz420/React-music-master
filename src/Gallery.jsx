7d85bc535d3b81cc0517e912aba53275f1788888
abiduzz420 <abiduzair420@gmail.com>
2017-05-25 12:14:39 +0530

    audio playable feature added

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Gallery extends Component {
+
+  playAudio(previewURL){
+    let audio = new Audio(previewURL);
+    audio.play();
+  }
   render() {
     const { tracks } = this.props;
     return(
@@ -9,19 +14,27 @@ class Gallery extends Component {
         {
           tracks.map((track,key) => {
             const trackImg = track.album.images[0].url
-            return(
-              <div
-                className="Track"
-                key={key}>
-                <img
-                  src = {trackImg}
-                  alt="track"
-                  className="Track-image"/>
-                <p className="Track-text">
-                  {track.name}
-                </p>
-              </div>
-            )
+            if(track.preview_url){
+              return(
+                <div
+                  className="Track"
+                  key={key}
+                  onClick={() => this.playAudio(track.preview_url)}>
+                  <img
+                    src = {trackImg}
+                    alt="track"
+                    className="Track-image"/>
+                  <p className="Track-text">
+                    {track.name}
+                  </p>
+                </div>
+              )
+            }
+            else{
+              return(
+                <div></div>
+              )
+            }
           })
         }
       </div>
