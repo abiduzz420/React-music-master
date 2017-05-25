70fcd280c42af7bbb65fa1c3319bc7a74a79d29d
abiduzz420 <abiduzair420@gmail.com>
2017-05-25 08:34:06 +0530

    fetching url done and profile component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React,{Component} from 'react';
 import './App.css';
 import {FormControl,FormGroup,InputGroup,Glyphicon} from 'react-bootstrap';
+import Profile from './Profile';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query:'',
+      artist: null
+    };
+  }
+
+  submit(){
+    const BASE_URL = 'https://api.spotify.com/v1/search?'
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    fetch(FETCH_URL,{
+      method:'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0]
+      this.setState({artist})
+    });
+
+  }
 
   render() {
     return (
@@ -10,21 +32,26 @@ class App extends Component {
         <div className="App-title">Masti Muzik</div>
         <FormGroup>
           <InputGroup>
-            <FormControl type="text" placeholder="Search an Artist..."/>
+            <FormControl type="text" placeholder="Search an Artist..."
+              value={this.state.query}
+              onChange={(event) => this.setState({query:event.target.value})}
+              onKeyPress={(event) => {
+                if(event.key === 'Enter'){
+                  this.submit()
+                }
+              }}/>
             <InputGroup.Addon>
-              <Glyphicon glyph="search"/>
+              <Glyphicon glyph="search"
+                onClick={()=>this.submit()}
+              />
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <div className="Profile">
-          <div>Artist Image</div>
-          <div>Artist Name</div>
-        </div>
+        <Profile artist={this.state.artist}/>
         <div className="Gallery">Gallery</div>
       </div>
-    );
+                );
+              }
+
   }
-
-}
-
 export default App;
---
+++ src/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class Profile extends React.Component {
+  render() {
+    // console.log('this.props',this.props);
+    return(
+      <div>
+        <div>Profile Image</div>
+        <div>Profile Name</div>
+      </div>
+    )
+  }
+}
+
+export default Profile
